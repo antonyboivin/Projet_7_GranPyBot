ada247a673b00f81baf9d6234c75171948224275
antonyboivin <antonyboivin@outlook.fr>
2018-04-29 00:41:41 +0200

    Display homme page whith jquery and js

--- granPyApp/parser.py
+++ granPyApp/parser.py
@@ -1,25 +1,6 @@
 import json
 
 
-testclass = "connais tu OpenClassrooms à Paris ?"
-
-
-def clean_user_request(user_request):
-    """
-        Analyze the user's request to keep only useful words.
-    """
-
-    user_request = user_request.lower()
-    user_request = user_request.split(" ")
-
-    with open("gpb_stopwords.json") as stopWordsFile:
-        gpb_stopwords = json.load(stopWordsFile)
-    for word in gpb_stopwords:
-        if word in user_request:
-            user_request.remove(word)
-    print(user_request)
-
-
 class Parser:
     def __init__(self, user_request):
         self.user_request = user_request
@@ -32,13 +13,37 @@ class Parser:
         self.user_request = self.user_request.lower()
         self.user_request = self.user_request.split(" ")
 
-        with open("gpb_stopwords.json") as stopWordsFile:
+        with open("granPyApp/gpb_stopwords.json") as stopWordsFile:
             gpb_stopwords = json.load(stopWordsFile)
         for word in gpb_stopwords:
             if word in self.user_request:
                 self.user_request.remove(word)
         return self.user_request
+        
 
+"""
+testclass = "connais tu OpenClassrooms à Paris ?"
+
+
+def clean_user_request(user_request):
+    #Analyze the user s request to keep only useful words.
+
+    user_request = user_request.lower()
+    user_request = user_request.split(" ")
+
+    with open("gpb_stopwords.json") as stopWordsFile:
+        gpb_stopwords = json.load(stopWordsFile)
+    for word in gpb_stopwords:
+        if word in user_request:
+            user_request.remove(word)
+    print(user_request)
+
+testclass = "connais tu OpenClassrooms à Paris ?"
 #clean_user_request(testclass)
 test = Parser(testclass)
 print(test.clean_user_request())
+"""
+
+
+
+
--- granPyApp/views.py
+++ granPyApp/views.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from .parser import Parser
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index/')
+#@app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/userRequest', methods=['POST'])
+def analysis():
+    userRequest = Parser(request.form['userRequest'])
+    userRequest = userRequest.clean_user_request()
+    return jsonify(userRequest)
 
 
 if __name__ == "__main__":
